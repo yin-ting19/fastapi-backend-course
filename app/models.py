5958c59ac76fbb28f193332a6a98242e26e7edd6
yunaaaa-aaa <yuna094516@gmail.com>
2024-12-12 15:44:30 +0800

    Add users table

--- app/models.py
+++ app/models.py
@@ -9,3 +9,11 @@ class Todo(Base):
     description = Column(String, nullable=True)
     completed = Column(Boolean, default=False)
     due_date = Column(Date, nullable=True)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id =   Column(Integer, primary_key=True)
+    username =  Column(String(50), nullable=False, unique=True)#最多 50 字元，必填且唯一
+    password = Column(String(255), nullable=False)#最多 255 字元，必填
+    email = Column(String(100), nullable=True, unique=True)#最多 100 字元，必填且唯一
