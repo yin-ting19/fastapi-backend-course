56f441a8fd1b5d6345c404028ed1f4deddc3b834
yin-ting19 <milk0903358833@gmail.com>
2024-12-19 15:54:46 +0800

    Update users table

--- alembic/env.py
+++ alembic/env.py
@@ -3,12 +3,10 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
-import sys
-from pathlib import Path
-sys.path.append(str(Path(__file__).resolve().parents[1]))
-from app.main import Base
-
 from alembic import context
+
+from app.database import Base
+from app.models import Todo
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
