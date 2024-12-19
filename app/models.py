1a3af960d34dcbd9159cbeafabd11f548e9fde63
yin-ting19 <milk0903358833@gmail.com>
2024-12-19 15:56:54 +0800

    Add todo priority

--- app/models.py
+++ app/models.py
@@ -9,10 +9,10 @@ class Todo(Base):
     description = Column(String, nullable=True)
     completed = Column(Boolean, default=False)
     due_date = Column(Date, nullable=True)
+    priority = Column(Integer, default=1)
 
 class User(Base):
     __tablename__ = "users"
-
     id =   Column(Integer, primary_key=True, index=True)
     username =  Column(String, nullable=False)
     password = Column(String, nullable=False)
